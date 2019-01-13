c194c5d23df54432608af65a9f508ee135921766
mlubin <lubinmichael86@gmail.com>
2019-01-13 15:24:19 -0500

    updating .js

--- static/js/app.js
+++ static/js/app.js
@@ -1,16 +1,20 @@
 function buildMetadata(sample) {
 
   // @TODO: Complete the following function that builds the metadata panel
-
+  var url = "/metadata/" + sample;
   // Use `d3.json` to fetch the metadata for a sample
     // Use d3 to select the panel with id of `#sample-metadata`
-
+  var panel = d3.select("#sample-metadata");
     // Use `.html("") to clear any existing metadata
-
+  panel.html ("");
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
-
+  d3.json(url).then(function (data) {
+    Object.defineProperties(data).forEach(([key, value]) => {
+      panel.append("h6").text('${key}: ${value}');
+    })
+  })
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
 }
@@ -18,13 +22,42 @@ function buildMetadata(sample) {
 function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
-
+  var chartURL = "/samples/" + sample;
+  d3.json(chartURL).then(function (data) {
     // @TODO: Build a Bubble Chart using the sample data
-
+  var trace1 = {
+    x: data.otu_ids,
+    y: data.sample_values,
+    mode: 'markers',
+    text: data.otu_labels,
+    marker: {
+      color: data.otu_ids,
+      size: data.sample_values,
+      colorscale: "Earth"
+    }
+  };
+  var trace1 =[trace1];
+  var layout = {
+    showlegend:false,
+    height: 600,
+    width: 1500
+  };
+  Plotly.newPlot('bubble', trace1, layout);
     // @TODO: Build a Pie Chart
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
+  var data = [{
+    values: data.sample_values.slice(0,10),
+    labels: data.otu_ids.slice(0,10),
+    hovertext: data.otu_labels.slice(0,10),
+    type: 'pie',
+  }];
+  var layout = {
+    showlegend: true,
+  };
+  Plotly.newPlot('pie', data, layout);
 }
+  )}
 
 function init() {
   // Grab a reference to the dropdown select element
