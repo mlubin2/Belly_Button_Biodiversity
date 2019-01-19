b2940a4bf804d1a0622731d8f678ba3c14bb02ab
mlubin <lubinmichael86@gmail.com>
2019-01-19 18:44:19 -0500

    uploading

--- static/js/app.js
+++ static/js/app.js
@@ -1,63 +1,131 @@
 function buildMetadata(sample) {
 
   // @TODO: Complete the following function that builds the metadata panel
-  var url = "/metadata/" + sample;
+
   // Use `d3.json` to fetch the metadata for a sample
+  var metadataURL = "/metadata/" + sample;
     // Use d3 to select the panel with id of `#sample-metadata`
-  var panel = d3.select("#sample-metadata");
+  var panelMetadata = d3.select("#sample-metadata");
     // Use `.html("") to clear any existing metadata
-  panel.html ("");
+  panelMetadata.html ("");
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
-  d3.json(url).then(function (data) {
-    Object.defineProperties(data).forEach(([key, value]) => {
-      panel.append("h6").text('${key}: ${value}');
-    })
-  })
+    d3.json(metadataURL).then(function (data) {
+      Object.entries(data).forEach(([key, value]) => {
+        panelMetadata.append("h6").text(`${key}: ${value}`
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
+  );
+})
+})
 }
 
 function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
-  var chartURL = "/samples/" + sample;
-  d3.json(chartURL).then(function (data) {
+  var chartsURL = "/samples/" + sample;
+  d3.json(chartsURL).then(function (data) {
     // @TODO: Build a Bubble Chart using the sample data
-  var trace1 = {
-    x: data.otu_ids,
-    y: data.sample_values,
-    mode: 'markers',
-    text: data.otu_labels,
-    marker: {
-      color: data.otu_ids,
-      size: data.sample_values,
-      colorscale: "Earth"
-    }
-  };
-  var trace1 =[trace1];
-  var layout = {
-    showlegend:false,
-    height: 600,
-    width: 1500
-  };
-  Plotly.newPlot('bubble', trace1, layout);
+    var trace1 = {
+      x: data.otu_ids,
+      y: data.sample_values,
+      mode: 'markers',
+      text: data.otu_labels,
+      marker: {
+        color: data.otu_ids,
+        size: data.sample_values,
+    
+        colorscale: "Earth"
+      }
+    };
+    var trace1 = [trace1];
+    var layout = {
+      showlegend: false,
+      height: 600,
+      width: 1500
+    };
+    Plotly.newPlot('bubble', trace1, layout);
     // @TODO: Build a Pie Chart
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
-  var data = [{
-    values: data.sample_values.slice(0,10),
-    labels: data.otu_ids.slice(0,10),
-    hovertext: data.otu_labels.slice(0,10),
+    var data = [{
+      values: data.sample_values.slice(0, 10),
+      labels: data.otu_ids.slice(0, 10),
+      hovertext: data.otu_labels.slice(0, 10),
+      type: 'pie',
+    }];
+    var layout = {
+      showlegend: true,
+    };
+    Plotly.newPlot('pie', data, layout);
+  }
+)}
+  //####Gauge Chart#####
+  var level = 175;
+  // trig to calc meter point
+  var degrees = 180 - level,
+    radius = .5;
+  var radians = degrees * Math.PI / 180;
+  var x = radius * Math.cos(radians);
+  var y = radius * Math.sin(radians);
+  //Path: change this to create a better triangle
+  var mainPath = 'M -.0 -0.025 L .0 0.025 L',
+    pathX = String(data.otu_ids),
+    space = ' ',
+    pathY = String(data.sample_values),
+    pathEnd = ' Z';
+  var path = 
+  mainPath.concat(pathX,space,pathY,pathEnd);
+
+  var data = [{ type: 'scatter',
+    x: [0], y:[0],
+    marker: {size: 28, color:'850000'},
+    showlegend: false,
+    name: 'gauge',
+    text: level,
+    hoverinfo: 'data.otu_labels'},
+  { values: [50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50],
+    rotation: 90,
+    text: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4', '2-3', '1-2', '0-1', ''],
+    textinfo: 'text',
+    textpostion: 'inside',
+    marker: {colors:[
+    'rgba(14, 127, 0, .5)',
+    'rgba(110, 154, 22, .5)',
+    'rgba(170, 202, 42, .5)',
+    'rgba(185, 205, 67, .5)', 
+    'rgba(202, 209, 95, .5)',
+    'rgba(210, 206, 145, .5)',
+    'rgba(220, 212, 180, .5)', 
+    'rgba(232, 226, 202, .5)',
+    'rgba(255, 255, 255, 0)']},
+    labels: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4', '2-3', '1-2', '0-1', ''],
+    hoverinfo: 'label',
+    hole: .5,
     type: 'pie',
+    showlegend: false
   }];
+
   var layout = {
-    showlegend: true,
+    shapes:[{
+      type: 'path',
+      path: path,
+      fillcolor: '850000',
+      line: {
+        color: '850000'
+      }
+    }],
+    title: 'gauge',
+    height: 500,
+    width: 500,
+    xaxis: {zeroline:false, showticketlabels:false,
+                showgrid: false, range: [-1, 1]},
+    yaxis: {zeroline:false, showticketlabels:false,
+                showgrid: false, range: [-1, 1]}
   };
-  Plotly.newPlot('pie', data, layout);
+  Plotly.newPlot('gauge', data, layout);
 }
-  )}
 
 function init() {
   // Grab a reference to the dropdown select element
